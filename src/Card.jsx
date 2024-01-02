23e8cd70ec494218e1b04434955c3e088289364e
jawad gouahmane <j.gouahmane@gmail.com>
2024-01-02 15:56:55 +0100

    payers data

---
+++ src/Card.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Data } from './Data'
+export default function Card( props) {
+
+ const datashow = ()=> Data.map( products=>{ 
+
+return ( 
+
+    <div className=''>
+
+
+<div class="product-card">   
+    <img src={products.img} alt="Product Image"/>
+    <div class="product-details">
+        <div class="product-title"><h4>{products.Name}</h4></div>
+        <div class="product-description">{products.Team}</div>
+        <div class="product-price">{products.Nationality}</div>
+        <div class="product-price">{products.jerseyNumber}</div>
+        <div class="product-price">{products.Age}</div>
+        <a href="#" class="buy-button">{products.Add}</a>
+    </div>
+</div> 
+
+    </div> 
+
+)
+
+  })
+ 
+  return (
+    <div className='' >   
+        <h1> Players  </h1>
+        <div className='container'>
+        {datashow()}
+        </div>
+    </div>
+  )
+}
---
+++ src/Data.js
@@ -0,0 +1,38 @@
+export const Data = [{
+    img : require('./image/banoun.jpg'),
+    Name : ' Badr Benoun',
+    Team  : ' Raja Athletic',
+    Nationality:  ' Morocco',
+    jerseyNumber: ' 13',
+    Age : 30,
+    Add : 'Acheter' , 
+},
+{
+    img : require('./image/malongo.webp'),
+    Name : ' Name: Ben  Malongo',
+    Team  : 'Team : Raja Athletic',
+    Nationality:   'Nationality: Congo RDC',
+    jerseyNumber: 'jerseyNumber: 28',
+    Age : " Age: 29",
+    Add : 'Acheter' , 
+},
+{
+    img : require('./image/milito.jpg'),
+    Name : 'Name:Diego milito',
+    Team  : 'Team: Inter Milan',
+    Nationality:  ' Nationality: Argentine',
+    jerseyNumber: 'jerseyNumber:22',
+    Age : "Age: 27",
+    Add : 'Acheter'  ,
+},
+{
+    img : require('./image/quaresma.jpg'),
+    Name : 'Name :Quaresma',
+    Team  : 'Team: Besiktas',
+    Nationality:  'Nationality: Portugal',
+    jerseyNumber: 'jerseyNumber: 77',
+    Age : "Age:30",
+    Add : 'Acheter' , 
+}
+]
+
